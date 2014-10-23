35473bb857a112ab6592fbae14a8aca9744bb296
Gustavo Mendonca <guzzgm@gmail.com>
2014-10-22 20:24:27 -0500

    Multithreading Java not going well :(

--- java/WordCount.java
+++ java/WordCount.java
@@ -1,13 +1,14 @@
 import java.util.*;
 import java.util.regex.*;
 import java.io.*;
+import java.util.concurrent.*;
 
 public class WordCount
 {
 
     public static ArrayList<ArrayList<String>> splitFile(File f, int numThreads) throws FileNotFoundException{
 
-    	int it = 1;
+    	int it = 0;
     	ArrayList<String> lines = new ArrayList<String>();
 
     	Scanner in = new Scanner(f);
@@ -26,11 +27,12 @@ public class WordCount
 
     	for(int i = 0; i < lines.size(); i++){
     		files.get(it).add(lines.get(i));
-    		if(it <= numThreads && i == (i*linePerFile)) it++;
+    		if(it <= numThreads && i != 0 &&i == (i*linePerFile)){
+    			it++;
+    		}
     	}
 
     	return files;
-
     }
 
 	public static void main(String[ ] args){
@@ -48,12 +50,22 @@ public class WordCount
 
 			files = splitFile(f, numThreads);
 
+			ArrayList<CountWord> maps = new ArrayList<CountWord>();
+
+			ExecutorService es = Executors.newCachedThreadPool();
 			for(int i = 0; i < numThreads; i++){
 				Map<String, Integer> wordCounts = new TreeMap<String, Integer>();
-				new CountWord(files.get(i), wordCounts, i).start();
+				CountWord cw = new CountWord(files.get(i), wordCounts, i);
+				es.execute(cw);
+				maps.add(cw);
 			}
+			es.shutdown();
+			boolean finshed = es.awaitTermination(60, TimeUnit.MINUTES);
 
-		}catch(FileNotFoundException e)
+			Thread m = new Thread(new Merge(maps));
+			m.start();
+
+		}catch(Exception e)
 		{
 			System.out.println(inputFileName + " not found!");
 			e.printStackTrace();
@@ -62,7 +74,7 @@ public class WordCount
 	}
 }
 
-class CountWord extends Thread
+class CountWord implements Runnable
 {
 	private ArrayList<String> inputFileName;
 	private Map<String, Integer> wordCounts;
@@ -72,6 +84,10 @@ class CountWord extends Thread
 		this.inputFileName = inputFileName;
 		this.wordCounts = wordCounts;
 		this.id = id;
+   	}
+
+   	public Map<String, Integer> getWordCountMap(){
+   		return wordCounts;
    	}
 
    	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
@@ -121,7 +137,7 @@ class CountWord extends Thread
 	public void run(){
 		int count;
       	String word;
-      	
+
 		for(String s : inputFileName){
 			String[] words = s.split("\\s+");
 			for(String w : words){
@@ -136,5 +152,64 @@ class CountWord extends Thread
 		}
 
 		WriteToFile();
+	}
+}
+
+class Merge implements Runnable
+{
+	private ArrayList<CountWord> maps;
+	private Map<String, Integer> wordCounts;
+
+	public Merge(ArrayList<CountWord> maps){
+		this.maps = maps;
+	}
+
+	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
+
+		List<Map.Entry<String, Integer>> list = 
+			new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1,
+                                           Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext();) {
+			Map.Entry<String, Integer> entry = it.next();
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		return sortedMap;
+	}
+
+	public void run(){
+
+		wordCounts = new TreeMap<String, Integer>();
+		for(CountWord cw : maps){
+			wordCounts.putAll(cw.getWordCountMap());
+		}
+
+		try {
+ 
+			File file = new File("output/wordcount-java-merged.txt");
+ 
+			if (!file.exists()) file.createNewFile();
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(String word : sortByComparator(wordCounts).keySet()){
+        		bw.write(new String(word + ": " + wordCounts.get(word) + "\n"));
+     		}
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
 	}
 }
